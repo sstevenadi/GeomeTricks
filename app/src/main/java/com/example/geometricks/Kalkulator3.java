8e0a69031b2abd861528d64ca6fb45e72c587e79
puppets29 <102816289+puppets29@users.noreply.github.com>
2022-04-05 19:43:42 +0700

    kalkulator layout

---
+++ app/src/main/java/com/example/geometricks/Kalkulator3.java
@@ -0,0 +1,14 @@
+package com.example.geometricks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kalkulator3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kalkulator3);
+    }
+}
---
+++ app/src/main/java/com/example/geometricks/Rumus2.java
@@ -0,0 +1,14 @@
+package com.example.geometricks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rumus2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rumus2);
+    }
+}
