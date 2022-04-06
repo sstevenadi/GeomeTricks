649c82d103e57ad39bf985af9c0bad5ef663d1a7
puppets29 <102816289+puppets29@users.noreply.github.com>
2022-04-06 10:34:22 +0700

    activiy tanya tutor layout

--- app/src/main/java/com/example/geometricks/Beranda.java
+++ app/src/main/java/com/example/geometricks/Beranda.java
@@ -9,11 +9,20 @@ import android.widget.ImageView;
 
 public class Beranda extends AppCompatActivity {
 ImageView button;
+ImageView button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
         button=findViewById(R.id.mat1);
+        button1=findViewById(R.id.teach);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1=new Intent(Beranda.this,Tanyatutor .class);
+                startActivity(intent1);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/geometricks/Kelas.java
+++ app/src/main/java/com/example/geometricks/Kelas.java
@@ -9,20 +9,25 @@ import android.widget.ImageView;
 
 public class Kelas extends AppCompatActivity {
 ImageView button;
-
+ImageView button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kelas);
         button=findViewById(R.id.imageView5);
-        button=findViewById(R.id.imageView14);
+        button1=findViewById(R.id.imageView14);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1=new Intent(Kelas.this,Beranda .class);
+                startActivity(intent1);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Kelas.this,Beranda .class);
-                Intent intent1=new Intent(Kelas.this,Beranda .class);
                 startActivity(intent);
-                startActivity(intent1);
 
             }
         });
